67fa3bf8501b5e7656f160ad8675e78b12ec9740
Daniela1026 <cartagenad49@gmail.com>
2023-01-11 08:46:11 -0500

    update

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
 apt-get update && apt-get install -y hugo make
 make build
