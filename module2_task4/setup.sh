8edc12f6932e59649166a32d3b630df95926c0c9
Daniela1026 <cartagenad49@gmail.com>
2023-01-20 19:36:27 -0500

    task4

--- module2_task4/setup.sh
+++ module2_task4/setup.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+#!/usr/bin/bash
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
 make build
