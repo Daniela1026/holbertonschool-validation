14b41f6797244f07cd718454e3de77d8a89de695
Daniela1026 <cartagenad49@gmail.com>
2023-01-24 16:47:23 -0500

    updatetask0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-## Production Instructions
-
-#* The production website is hosted in an Ubuntu 18.04 Docker container
-#* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
-sudo apt update
-sudo apt-get update && apt-get install -y make
-sudo apt install -y curl
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-sudo apt install ./hugo.deb
-
-#* When running the command `make build`, there is a bunch of errors which end with the following lines:
-make build
+rm -rf /usr/local/go
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+apt install ./last.deb
+rm last.deb
