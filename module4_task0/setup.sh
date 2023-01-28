dcc6d9338f4807e07ace26de561cd0c0d54f99bf
Daniela1026 <cartagenad49@gmail.com>
2023-01-28 11:53:23 -0500

    task0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-rm -f hugo_extended_0.109.0_Linux-64bit.tar.gz || true
-
-apt-get install zip -y
-npm install -g markdownlint-cli -y
-
-make build
-
-sudo apt-get remove golang-go
-rm -rf /usr/local/go || true
-
-rm -rf dist/ 2> /dev/null
-rm -f awesome-api || true
-rm -f coverage-units.out || true
-rm -f coverage-integrations.out || true
+sudo apt-get install -y apt-utils
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.91.2/hugo_0.91.2_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+npm install -g markdownlint-cli
+rm hugo.deb
