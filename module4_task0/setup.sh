fbe2901aef35ddcdd717461c5a2580b15af56e24
Daniela1026 <cartagenad49@gmail.com>
2023-01-28 12:01:59 -0500

    task0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install -y apt-utils
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.91.2/hugo_0.91.2_Linux-64bit.deb -o hugo.deb
-sudo apt install ./hugo.deb
-npm install -g markdownlint-cli
-rm hugo.deb
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+rm -f hugo_extended_0.109.0_linux-amd64.deb
