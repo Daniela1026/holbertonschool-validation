011f88df001de148f1d5f1d56b9df01a202a7665
Daniela1026 <cartagenad49@gmail.com>
2023-01-28 15:45:11 -0500

    task3

--- module4_task3/setup.sh
+++ module4_task3/setup.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-npm install -g markdownlint-cli -y
-
-sudo apt-get remove golang-go
-rm -rf /usr/local/go || true
-
-rm -rf dist/ 2> /dev/null
-rm -f awesome-api || true
-rm -f coverage-units.out || true
-rm -f coverage-integrations.out || true
+apt-get update && apt-get install -y make
