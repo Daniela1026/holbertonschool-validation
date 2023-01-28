623fe77e0a3644dc85f26f4c6edf7a548ffb1ca4
Daniela1026 <cartagenad49@gmail.com>
2023-01-28 11:32:56 -0500

    task0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+sudo apt-get update
+sudo apt-get install -y make golang-go git curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugoversion.deb
+sudo apt install ./hugoversion.deb
+sudo snap install golangci-lint
+sudo gem install chef-utils -v 16.6.14
+sudo npm install -g markdownlint-cli
+sudo gem install mdl -v 0.11.0
